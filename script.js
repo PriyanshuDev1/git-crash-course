a9fc4f8bfb1ca25b97a6b54c1fa4712585ce5df7
Priyanshu Dev <priyanshudev037@gmail.com>
2024-01-22 09:45:33 +0530

    added isAbove18 function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const age = 17;
 
-const name = "Priyanshu";
+const name1 = "Priyanshu";
 const dob = "4/10/2006"
 
 
 
-function sayName(name){
-    console.log('My name is: '+name);
+function sayName(name1){
+    console.log('My name is: '+name1);
 }
--- utils/utils.js
+++ utils/utils.js
@@ -6,3 +6,7 @@ function add(a,b){
 }
 
 console.log(add(a,b));
+
+function isAbove18(){
+    return age>=18;
+}
